facfba14002855eb03254e187b81390d73157b27
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-19 16:26:32 -0300

    test: first test created

---
+++ src/usecases/users/create/create-user-usecase.spec.ts
@@ -0,0 +1,7 @@
+describe('CreateUserUseCase', () => {
+  it('should send a request to create an user in API and returns the created user', async () => {
+    const createdUser = '1'
+    const anyUser = '1'
+    expect(createdUser).toBe(anyUser)
+  })
+})
