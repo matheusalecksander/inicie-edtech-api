260f96236b3f99a5fc322c59d3347f7c9a48922c
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-19 16:55:37 -0300

    test: ensure perform method returns an user with id

--- src/core/entities/users/usecases/create-usecase.ts
+++ src/core/entities/users/usecases/create-usecase.ts
@@ -1,6 +1,6 @@
 import { UserData } from '../data/user-data'
 import { UserModel } from '../models/user-model'
 
-export interface CreateUserUsecase {
+export interface ICreateUserUsecase {
   perform(data: UserData): Promise<UserModel>
 }
--- src/usecases/users/create/create-user-usecase.spec.ts
+++ src/usecases/users/create/create-user-usecase.spec.ts
@@ -1,7 +1,27 @@
+import { UserData } from '../../../core/entities/users/data/user-data'
+import { UserModel } from '../../../core/entities/users/models/user-model'
+import { ICreateUserUsecase } from '../../../core/entities/users/usecases/create-usecase'
+
+class CreateUserUseCase implements ICreateUserUsecase {
+  async perform(data: UserData): Promise<UserModel> {
+    const user = data
+
+    return { ...user, id: 'any_id' }
+  }
+}
+
 describe('CreateUserUseCase', () => {
-  it('should send a request to create an user in API and returns the created user', async () => {
-    const createdUser = '1'
-    const anyUser = '1'
-    expect(createdUser).toBe(anyUser)
+  it('should returns the created user with gerenated id', async () => {
+    const sut = new CreateUserUseCase()
+    const anyUser: UserData = {
+      email: 'any_email@email.com',
+      gender: 'any_gender',
+      name: 'any_name',
+      status: 'any_status',
+    }
+
+    const createdUser = await sut.perform(anyUser)
+
+    expect(createdUser).toEqual({ ...anyUser, id: 'any_id' })
   })
 })
