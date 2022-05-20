92068cba770aa732a550789c260a14321c3df7f9
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-20 17:33:59 -0300

    chore: moved InMemoryUsersRepository to an folder users

---
+++ src/repositories/users/in-memory-users-repository.ts
@@ -0,0 +1,17 @@
+import { UserData } from '~/core/entities/users/data/user-data'
+import { UserModel } from '~/core/entities/users/models/user-model'
+import { IUsersRepository } from '~/core/entities/users/repository/users-repository'
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private readonly users: UserModel[] = []
+
+  async create(data: UserData): Promise<UserModel> {
+    const user = {
+      ...data,
+      id: 'any_id',
+    }
+    this.users.push(user)
+
+    return user
+  }
+}
