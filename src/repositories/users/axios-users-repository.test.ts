b5c5ba11f63be8ac58b68026e98802a92d88a36d
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-21 09:55:38 -0300

    test: mocked axios instance and ensure it has been called with correct values

--- src/repositories/users/axios-users-repository.test.ts
+++ src/repositories/users/axios-users-repository.test.ts
@@ -1,5 +1,36 @@
+import { UserData } from '~/core/entities/users/data/user-data'
+import { AxiosUsersRepository } from './axios-users-repository'
+import { axiosInstance } from '~/utils/db/axios'
+
+jest.mock('~/utils/db/axios')
+
+const mockedaxios = axiosInstance as jest.Mocked<typeof axiosInstance>
+
+function makeSut() {
+  const sut = new AxiosUsersRepository()
+
+  return sut
+}
+
 describe('Integration test', () => {
-  it('Script test', () => {
-    expect(1 + 1).toBe(2)
+  it('should post an user with correct values', async () => {
+    const sut = makeSut()
+    const expectedResult = {
+      email: 'any_email@email.com',
+      gender: 'any_gender',
+      name: 'any_name',
+      status: 'any_status',
+      id: 'any_id',
+    }
+    const anyUser: UserData = {
+      email: 'any_email@email.com',
+      gender: 'any_gender',
+      name: 'any_name',
+      status: 'any_status',
+    }
+    mockedaxios.post.mockResolvedValueOnce({ data: expectedResult })
+
+    await sut.create(anyUser)
+    expect(mockedaxios.post).toHaveBeenCalledWith('/users', anyUser)
   })
 })
