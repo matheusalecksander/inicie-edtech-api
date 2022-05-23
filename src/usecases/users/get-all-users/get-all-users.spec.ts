798eaed0f57284bc433e5c6fc991e38a3d77aa50
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-23 14:54:19 -0300

    feat: updated GetAllUsersUsecase to use repository to retrieve data

--- src/usecases/users/get-all-users/get-all-users.spec.ts
+++ src/usecases/users/get-all-users/get-all-users.spec.ts
@@ -1,16 +1,23 @@
-class GetAllUsersUsecase {
-  async perform() {
-    return [
-      {
-        name: 'matheus',
-      },
-    ]
+import { UserModel } from '../../../core/entities/users/models/user-model'
+import { IUsersRepository } from '../../../core/entities/users/repository/users-repository'
+import { IGetAllUsersUsecase } from '../../../core/entities/users/usecases/get-all-users-usecase'
+
+import { InMemoryUsersRepository } from '../../../repositories/users/in-memory-users-repository'
+
+class GetAllUsersUsecase implements IGetAllUsersUsecase {
+  constructor(private repository: IUsersRepository) {}
+
+  async perform(): Promise<UserModel[]> {
+    const users: UserModel[] = await this.repository.getAllUsers()
+
+    return users
   }
 }
 
 describe('GetAllUsersUsecase', () => {
   it('should call repository and returns an array with users data', async () => {
-    const sut = new GetAllUsersUsecase()
+    const repository = new InMemoryUsersRepository()
+    const sut = new GetAllUsersUsecase(repository)
 
     const users = await sut.perform()
     expect(users).toBeInstanceOf(Array)
