f6955bc9837f4a982ae49ab5d563254669e54530
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-23 17:52:38 -0300

    feat: created controller to users post route

--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { CreateUserController } from '../controllers/create-user-controller'
 
 const router = Router()
 
 router.get('/', (req, res) => {
   res.send('Hello from API')
 })
+
+router.post('/users', new CreateUserController().handle)
 
 export { router }
