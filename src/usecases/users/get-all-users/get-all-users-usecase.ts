2b454819b39c4ffed3c38c8a26f3f9a640fa3659
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-24 17:00:02 -0300

    refactor: removed trycatch block

--- src/usecases/users/get-all-users/get-all-users-usecase.ts
+++ src/usecases/users/get-all-users/get-all-users-usecase.ts
@@ -1,19 +1,13 @@
 import { UserModel } from '../../../core/entities/users/models/user-model'
 import { IUsersRepository } from '../../../core/entities/users/repository/users-repository'
 import { IGetAllUsersUsecase } from '../../../core/entities/users/usecases/get-all-users-usecase'
-import { InternalServerError } from '../../../utils/errors/internal-server-error'
 
 export class GetAllUsersUsecase implements IGetAllUsersUsecase {
   constructor(private repository: IUsersRepository) {}
 
   async perform(): Promise<UserModel[]> {
-    try {
-      const users: UserModel[] = await this.repository.getAllUsers()
+    const users: UserModel[] = await this.repository.getAllUsers()
 
-      return users
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      throw new InternalServerError(error.message)
-    }
+    return users
   }
 }
