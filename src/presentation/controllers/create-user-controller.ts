f1f8a20d5b0a292b988ea553a7df0c7bc668cc28
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-24 15:00:25 -0300

    refactor: changed body destructuring, added trycatch block and update response on errors

--- src/presentation/controllers/create-user-controller.ts
+++ src/presentation/controllers/create-user-controller.ts
@@ -7,15 +7,21 @@ import { IController } from '../protocols/controllers'
 
 export class CreateUserController implements IController {
   async handle(req: Request, res: Response): Promise<void> {
-    const data: UserData = req.body
+    const { ...data }: UserData = req.body
+    try {
+      const repository = new AxiosUsersRepository()
+      const validator = new ObjectValidator()
+      const usecase = new CreateUserUseCase(repository, validator)
+      const service = await usecase.perform(data)
 
-    const repository = new AxiosUsersRepository()
-    const validator = new ObjectValidator()
-    const usecase = new CreateUserUseCase(repository, validator)
-
-    const service = await usecase.perform(data)
-    console.log(service)
-
-    res.status(201).json(data).end()
+      res.json(service).status(201).end()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      res.status(error.status).json({
+        error: error.name,
+        msg: error.msg,
+        data: error.data,
+      })
+    }
   }
 }
